14c4f25c6d4fbe156d22c0fc77a64e236c7d6bfd
Renee Nordholm <reneenordholm@gmail.com>
2019-05-11 17:30:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,14 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand(size)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+  
+  def brands(brands)
+    BRANDS << brand.size
   end
 end
