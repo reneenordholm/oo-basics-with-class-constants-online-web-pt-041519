d83a7783c0b80bb0d5d3385747926968d282bbf9
Renee Nordholm <reneenordholm@gmail.com>
2019-05-11 17:17:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,12 @@
 class Shoe
-  attr_accessor :color, :material, :condition
-  attr_reader :brand, :size
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    BRANDS << brand.size
   end
 
   def cobble
@@ -14,7 +14,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def size=(size)
-    @size = size
-  end
 end
